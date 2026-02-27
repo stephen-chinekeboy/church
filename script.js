f1ad92e8b5da9048db203614323258e44d6f00f8
stephen-chinekeboy <idokostephen93@gmail.com>
2026-02-27 14:58:53 +0100

    responsive navbar

--- script.js
+++ script.js
@@ -6,3 +6,16 @@
       link.classList.add("active");
     }
   });
+
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('ul');
+
+  hamburger.addEventListener('click' , () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+  });
+
+document.querySelectorAll('a').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
